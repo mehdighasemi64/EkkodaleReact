38f28abe377038b6f6adb1ccda837da08f8b4f33
MEHDI <mehdi.ghasemi64@gmail.com>
2022-06-21 08:08:56 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {  Client, Movie, Person } from "./Ekkodalets";
 
-function App() {
+function App() 
+{
+  const [PersonList, setPersonList] = React.useState<Person[] | null>();
+  const [MovieList, setMovieList] = React.useState<Movie[] | null>();
+
+  React.useEffect(() => {    
+    loadPersons();
+    loadMovies();
+  }, [setPersonList, setMovieList]);
+
+  const EkkodaleClient = new Client("https://localhost:7200");
+
+  async function loadPersons() 
+  {
+    const PersonList = await EkkodaleClient.personNeo4JAll();
+    setPersonList(PersonList);
+  }
+
+  async function loadMovies() 
+  {
+    const MovieList = await EkkodaleClient.movieNeo4JAll();
+    setMovieList(MovieList);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {PersonList ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Family</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {PersonList.map(({ personID, name, family, age, fullName }) => (
+                <tr key={personID}>
+                  <td>{name}</td>
+                  <td>{family}</td>
+                  <td>{age}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Loading Persons...</p>
+        )}
+        <div> ---------------------------------------------------------------</div>
+      {MovieList ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th style={{paddingLeft:"100px"}}>Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              {MovieList.map(({ movieID, title , year }) => (
+                <tr key={movieID}>
+                  <td>{year}</td>
+                  <td style={{paddingLeft:"100px"}}>{title}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Loading Movies...</p>
+        )}
+        
       </header>
     </div>
   );
